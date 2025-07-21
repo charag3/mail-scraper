fbd7a70cd160fed347ceb336c1eb19704df2dd47
charag3 <char.agcreates@outlook.com>
2025-07-21 14:23:58 -0600

    Facebook commit 2

--- index.js
+++ index.js
@@ -95,7 +95,10 @@ function findFacebookLink($) {
 
 async function scrapeFacebook(fbUrl) {
     console.log(`🧭 Scraping Facebook page: ${fbUrl}`);
-    const browser = await puppeteer.launch({ headless: true });
+    const browser = await puppeteer.launch({ 
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
     const page = await browser.newPage();
     try {
         await page.goto(fbUrl, { waitUntil: 'networkidle2', timeout: 20000 });
